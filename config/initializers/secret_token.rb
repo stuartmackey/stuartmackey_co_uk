ec8f33d221593b22e4ce2653814a97f33b8aac03
Stuart Mackey <stuart@paperbagltd.com>
2013-01-22 14:13:06 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestBloggy::Application.config.secret_token = '9d73e51fd9ce2fa9661684b2f6946d8a9adb17d1a774079828827ba4602a69a75503fcd2525c70a6106df44e057f7ed507798cf0ff3ea4da1dc189ab1e875b11'
---
+++ config/jekyll/_posts/2013-01-22-another-test.markdown
@@ -0,0 +1,6 @@
+---
+layout: post
+published: true
+title: another-test
+---
+Just a quick test of bloggy
